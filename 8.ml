97a739e6c92d6183ad82de751e8c3eac0778e002
JimmyTanUWA <165866845+JimmyTanUWA@users.noreply.github.com>
2024-08-31 15:27:03 +0800

    file to be tested on

---
+++ 8.ml
@@ -0,0 +1,14 @@
+# 9 is printed
+#
+one <- 1
+#
+function increment value
+	x <- 3
+	return value + one + x
+#
+print increment(3) + increment(4)
+
+function multiply a b
+	x <- a * b
+	return x
+print multiply(10, 5)
